2b0dccf4417aeeab7e2a8082ced386dd6bca5538
Ric Rodriguez <rirrodri@ucsc.edu>
2017-05-25 14:25:28 -0700

    Init

--- gain.C
+++ gain.C
@@ -1,10 +1,9 @@
 {
 
-	gSystem->Load("~/Downloads/TCTAnalyse.V2.0/TCTAnalyse.sl");
-	
+  gSystem->Load("~/Downloads/TCTAnalyse.V2.0/TCTAnalyse.sl");
+
   char * file = (char *) "./example.rtct";
-	
   gROOT->ProcessLine(".x LoadPSTCT.C");
-	stct.PrintInfo();
+  stct.PrintInfo();
   stct.Draw(1,0,0,0,0,0,65,85)->Draw("SURF2Z");
 }
