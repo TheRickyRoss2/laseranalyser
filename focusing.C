3cee2f270ba0d0a4e3b3f5344164d4af7d03873a
TheRickyRoss2 <rirrodri@ucsc.edu>
2017-06-22 16:12:06 -0700

    Updated axes

--- focusing.C
+++ focusing.C
@@ -4,36 +4,96 @@
   * Version: 1.0
   * Author: Ric Rodriguez
   * Function: Calculates the focal point optical coordinate and beam spot size
-  * 
-  *
   ****************************************************************************/
 
-  #define CHANNEL CH4
-  #define DEBUG 1
-  #define FILEPATH "Run6_HPK80D_KU_RED.rtct"
+  // User defined variables
+  #define CHANNEL CH4 // Select which channel the pad is linked to
+  #define FILEPATH "Run6_HPK80D_KU_RED.rtct" // Path to PSTCT waveform file
+  #define SCANAXIS X // Scanning axis for waveforms
+  #define t0 60 // Approx start time of pulse
+  #define t1 80 // Approx end time of pulse
 
+  // Aux macros
+  #define time(X) (1024*(X)/200) // DRS4 time->bin conversion
+
+
+  // Load in waveform file
   gROOT->ProcessLine("gErrorIgnoreLevel=2001");
   char* file = (char *)  FILEPATH;
   PSTCT meas(file,0,2);
   meas.PrintInfo();
   enum{
+    X = 0;
+    Y = 1;
+    } axis;
+
+  enum	  {
     CH1=0,
     CH2=1,
     CH3=2,
     CH4=3
   } channel;
 
+  // Set up input data buffers
   int i=0, j=0;
-  int x0=0, z0=0;
-  int *dx;
-  dx = (int *) malloc(sizeof(int)*meas.Nz);
+  int axis0=0, z0=0;
+  int *dAxis
+  dAxis = (int *) malloc(sizeof(int)*meas.Nz);
   bool foundStart=false, foundEnd=false;
-  double startSignal = -1*0.1*meas.GetHA(CHANNEL, meas.Nx*3/4, 0, meas.Nz/2, 0, 0)->GetMinimum(); 
-  double fullSignal = -1*0.9*meas.GetHA(CHANNEL, meas.Nx*3/4, 0, meas.Nz/2, 0, 0)->GetMinimum(); 
+
+  // Judiciously choose a waveform which the laser hits entirely as a reference
+  // In this case we assume that the scan is from on the metal to off the metal
+  // We also assume that our focusing axis is Z
+  double startSignal = 0, fullSignal = 0;
+  switch (SCANAXIS) {
+  case X: 
+	  startSignal = -1 * 0.1 * meas.GetHA(CHANNEL, meas.Nx * 3 / 4, 0, meas.Nz / 2, 0, 0)->GetMinimum();
+	  fullSignal = -1 * 0.9 * meas.GetHA(CHANNEL, meas.Nx * 3 / 4, 0, meas.Nz / 2, 0, 0)->GetMinimum();
+	  break;
+  case Y:
+	  startSignal = -1 * 0.1 * meas.GetHA(CHANNEL, 0, meas.Ny * 3 / 4, meas.Nz / 2, 0, 0)->GetMinimum();
+	  fullSignal = -1 * 0.9 * meas.GetHA(CHANNEL, 0, meas.Ny * 3 / 4, meas.Nz / 2, 0, 0)->GetMinimum();
+	  break;
+  default:
+	  startSignal = 15;
+	  fullSignal = 120;
+  }
 
   for(i=0;i<meas.Nz;i++){
     foundStart = false;
     foundEnd = false;
+	switch (SCANAXIS) {
+	case X:
+		for (j = 0; j<meas.Nx; j++) {
+			TH1F * t1;
+			t1 = meas.GetHA(CHANNEL, j, 0, i, 0, 0);
+			t1->GetXaxis()->SetRange(time(t0), time(t1));
+			if (!foundStart) {
+				foundStart = startSignal <= -1 * t1->GetMinimum();
+				x0 = j;
+			}
+			if (!foundEnd) {
+				foundEnd = fullSignal <= -1 * t1->GetMinimum();
+				dx[i] = j - x0;
+			}
+		}
+	case Y:
+		for (j = 0; j<meas.Ny; j++) {
+			TH1F * t1;
+			t1 = meas.GetHA(CHANNEL, 0, j, i, 0, 0);
+			t1->GetXaxis()->SetRange(time(t0), time(t1);
+			if (!foundStart) {
+				foundStart = startSignal <= -1 * t1->GetMinimum();
+				axis0 = j;
+			}
+			if (!foundEnd) {
+				foundEnd = fullSignal <= -1 * t1->GetMinimum();
+				dAxis[i] = j - axis0;
+			}
+		}
+	default:
+
+	}
     for(j=0;j<meas.Nx;j++){
       TH1F * t1;
       t1 = meas.GetHA(CHANNEL, j, 0, i, 0, 0);
